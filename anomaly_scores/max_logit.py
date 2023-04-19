80a554622b900dfc7ddb4d1930fdb33775fe2468
EricKolibacz <26793186+EricKolibacz@users.noreply.github.com>
2023-04-19 17:36:23 +0000

    Add max logit anomly score

---
+++ anomaly_scores/max_logit.py
@@ -0,0 +1,11 @@
+"""Score by the maximum logit"""
+import torch
+
+from util.get_odd_score import to_np
+
+
+def max_logit_anomaly_score(output):
+    """Computes the anomly score by taking the max logit (similar to the paper by Hendrycks)"""
+    maxima, _ = torch.max(output, -1)
+    
+    return -1 * to_np(maxima)

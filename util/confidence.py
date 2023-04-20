af3fe7db0d9d2b5d0d1d13f7fe9c2f73fe50d129
EricKolibacz <26793186+EricKolibacz@users.noreply.github.com>
2023-04-20 14:51:58 +0000

    Introduce function to compute rms calibration error

--- util/confidence.py
+++ util/confidence.py
@@ -16,3 +16,35 @@ def bin_samples(keys: np.array, bins: np.array, values: np.array = None) -> list
     return binned_x
 
 
+def compute_rms_calibration_error(labels, predictions, bins=10) -> np.ndarray:
+    """Computes the root mean square calibration error
+
+    Args:
+        bins (List): a list of bins; each of them containing predictions (samples, softmax_output),
+                     and labels
+
+    Returns:
+        np.ndarray: root mean square calibration error
+    """
+
+    labels_binned = bin_samples(np.max(predictions, axis=1), np.linspace(0, 1, bins + 1), values=labels)
+    preds_binned = bin_samples(np.max(predictions, axis=1), np.linspace(0, 1, bins + 1), values=predictions)
+
+    total_samples = len(labels)
+
+    rms_calibration_error = 0
+    for labels, predictions in zip(labels_binned, preds_binned):
+        if labels.size == 0:  # empty
+            continue
+
+        assert len(predictions[:, 0]) == len(labels), "The predictions and labels don't have the same length"
+
+        bin_size = len(labels)
+        predicted_labels = np.argmax(predictions, axis=1)
+        correct_classified = labels == predicted_labels
+        ratio_correct = np.sum(correct_classified) / bin_size
+        mean_confidence = np.mean(np.max(predictions, axis=1))
+
+        rms_calibration_error += bin_size / total_samples * (ratio_correct - mean_confidence) ** 2
+
+    return np.sqrt(rms_calibration_error)
--- util/tests/test_confidence.py
+++ util/tests/test_confidence.py
@@ -26,7 +26,7 @@ def test_binning_empty_bin():
 def test_binning_edge_case():
     x = np.array([0.00001, 1, 0.00001])
     bins = np.linspace(0, 1, 4 + 1)
-    
+
     expected = [np.array([0.00001, 0.00001]), np.array([]), np.array([]), np.array([1])]
     actual = bin_samples(x, bins)
     for exp, act in zip(expected, actual):
@@ -43,3 +43,66 @@ def test_binning_different_keys_values():
     for exp, act in zip(expected, actual):
         np.testing.assert_array_equal(exp, act)
 
+
+def test_rms_calibration_error_perfect_binary():
+    labels = np.array([0, 0, 1, 1])
+    predictions = np.array(
+        [
+            [1.0, 0.0],
+            [1.0, 0.0],
+            [0.0, 1.0],
+            [0.0, 1.0],
+        ]
+    )
+    expected = 0
+
+    actual = compute_rms_calibration_error(labels, predictions)
+
+    assert expected == actual
+
+
+def test_rms_calibration_error_binary_calibrated():
+    labels = np.array([0, 0, 0, 1])
+    predictions = np.array(
+        [
+            [0.75, 0.25],
+            [0.75, 0.25],
+            [0.75, 0.25],
+            [0.75, 0.25],
+        ]
+    )
+    expected = 0
+
+    actual = compute_rms_calibration_error(labels, predictions)
+
+    assert expected == actual
+
+
+def test_rms_calibration_error_binary():
+    labels = np.array([0])
+    predictions = np.array([[0.75, 0.25]])
+    expected = 0.25
+
+    actual = compute_rms_calibration_error(labels, predictions)
+
+    assert expected == actual
+
+
+def test_rms_calibration_error_binary_2outputs():
+    labels = np.array([0, 1])
+    predictions = np.array([[0.75, 0.25], [0.75, 0.25]])
+    expected = 0.25
+
+    actual = compute_rms_calibration_error(labels, predictions)
+
+    assert expected == actual
+
+
+def test_rms_calibration_error_binary_2outputs():
+    labels = np.array([0, 0, 0])
+    predictions = np.array([[0.9, 0.1], [0.5, 0.499], [0.5, 0.499]])
+    expected = np.sqrt(1 / 3 * (0.9 - 1) ** 2 + 1 / 6 * (1 - 2) ** 2)
+
+    actual = compute_rms_calibration_error(labels, predictions)
+
+    assert expected == actual
